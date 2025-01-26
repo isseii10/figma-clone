a745abd0172e04064734b09bfd77def0a5b87c72
isseii10 <t1l.g2.o11g@gmail.com>
2025-01-27 08:16:33 +0900

    utils

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+import type React from "react";
+import { type Camera, type Point, type Color } from "./types";
+
+export const colorToCss = (color: Color) => {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
+};
+
+export const pointerEventToCanvasPoint = (
+  e: React.PointerEvent,
+  camera: Camera,
+): Point => {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
